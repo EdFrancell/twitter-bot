9af979b4c217707088a3b65510ac9650fca70cb8
eduardo <eduardo@aproveita.com.br>
2017-08-21 02:32:26 +0000

    fix config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {  
-  consumer_key: 'noKCZxt8BoSgakbQJgBtFHPXv',  
-  consumer_secret: '7lNbzeh0wwWBCPnbvolx0c8SLM0cCJxS05ngLM6CJbpabWzWKC',
-  access_token: '727591185137700864-P9d71kbi1nUJlVDXaQudcsAwIECDQ0X',  
-  access_token_secret: 'HDiGJkI4dFCgiuZY15NoMsQ7uAAmpStvoqMb9MVRt9oAw'
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
 }
 
